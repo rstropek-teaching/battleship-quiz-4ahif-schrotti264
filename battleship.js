f808df87e82d4594bce25f700ec68454ca8868b7
schrotti264 <31966880+schrotti264@users.noreply.github.com>
2017-09-30 16:56:15 +0200

    Finished Homework

--- battleship.js
+++ battleship.js
@@ -1,3 +1,10 @@
+var ships = [5,4,3,3,2];
+
+const NORTH = 0;
+const EAST = 1;
+const SOUTH = 2;
+const WEST = 3;
+
 $(() => {
   // Select table containing the battleground
   const battleground = $('#battleground');
@@ -19,12 +26,90 @@ $(() => {
   }
 
   $('#generate').click(() => {
-    // Here you have to add your code for building a random battleground.
+    console.log(0);
+    for(var i = 0; i < 10;i++){
+      console.log(8);
+      for(var j = 0; j < 10; j++){
+        console.log(9);
+        removeSquare(i,j);
+      }
+    }
+    console.log('x');
+    for(var i = 0; i < ships.length; i++){
+      console.log(1);
+      while(true){
+        console.log(2);
+        x = Math.floor(Math.random()*10);
+        y = Math.floor(Math.random()*10);
+        r = Math.floor(Math.random()*4);
+        if(putShip(ships[i],x,y,r)){
+          break;
+        }
+      }
+    }
 
-    // Tip: The next line of code demonstrates how you can select a table cell
-    // using coordinates, remove CSS classes and add CSS classes. 
-    $('td[data-r="1"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="2"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="3"][data-c="1"]').removeClass('water').addClass('ship');
   });
 });
+
+function putShip(shipLength=1,x=0,y=0,r=NORTH,firstCall=true){
+  if(isOccupied(x,y) || neighbourCount(x,y) > 1){
+    return false;
+  }
+  if(firstCall && neighbourCount(x,y) === 1){
+    return false;
+  }
+  if(x < 0 || x > 9 || y < 0 || y > 9){
+    return false
+  }
+  putSquare(x,y);
+  if(shipLength === 1){
+    // Last square of ship has been placed successfully (found spot for ship)
+    return true;
+  }
+  switch(r){
+    case NORTH:
+      var success = putShip(shipLength-1,x,y-1,r,false);
+      break;
+    case EAST:
+      var success = putShip(shipLength-1,x+1,y,r,false);
+      break;
+    case SOUTH:
+      var success = putShip(shipLength-1,x,y+1,r,false);
+      break;
+    case WEST:
+      var success = putShip(shipLength-1,x-1,y,r,false);
+      break;
+    default:
+      var success = false;
+  }
+  if(success){
+    return true;
+  }
+  removeSquare(x,y);
+  return false;
+}
+function isOccupied(x,y) {
+  return $('td[data-r="'+ y +'"][data-c="'+ x +'"]').hasClass('ship');
+}
+function neighbourCount(x,y) {
+  var result = 0;
+  if(isOccupied(x,y-1)){
+    result++;
+  }
+  if(isOccupied(x+1,y)){
+    result++;
+  }
+  if(isOccupied(x,y+1)){
+    result++;
+  }
+  if(isOccupied(x-1,y)){
+    result++;
+  }
+  return result;
+}
+function putSquare(x,y){
+  $('td[data-r="'+ y +'"][data-c="'+ x +'"]').removeClass('water').addClass('ship');
+}
+function removeSquare(x,y){
+  $('td[data-r="'+ y +'"][data-c="'+ x +'"]').removeClass('ship').addClass('water');
+}
